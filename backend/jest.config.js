8c508d44d843a23bc6276c0450df5a26a9abcfec
Karan124ec <karansinghec21@gmail.com>
2026-01-27 18:11:45 +0530

    chore(backend): Initialize backend configuration and dependencies

---
+++ backend/jest.config.js
@@ -0,0 +1,24 @@
+export default {
+  testEnvironment: 'node',
+  transform: {},
+  moduleFileExtensions: ['js', 'mjs'],
+  testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/config/database.js',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  verbose: true,
+  testTimeout: 10000,
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
+};
