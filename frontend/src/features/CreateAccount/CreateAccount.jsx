0ac54586cecde8486c4ed9c57f2caeece0c772a8
kunalksharma086 <kunalksharma086@gmail.com>
2026-01-27 17:16:23 +0530

    feat: Add admin pages (Dashboard, Users, Accounts, Transactions, KYC)

---
+++ frontend/src/features/CreateAccount/CreateAccount.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+import LoginImage from "../../images/bank.svg";
+import Card from "../../components/Card";
+import Navbar from "../../components/Navbar";
+import { useFormik } from "formik";
+import axios from "axios";
+
+import { API_BACKEND_URL } from "../../Context/Context";
+
+export default function CreateAccount() {
+  const [showSuccessMsg, setShowSuccessMsg] = React.useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    onSubmit: (values) => {
+      // Create an account
+      createAccount(values);
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.name) errors.name = "Name is required";
+      if (!values.email) {
+        errors.email = "Email is required";
+      } else {
+        if (
+          !values.email.match(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          )
+        ) {
+          errors.email = "Email address should be valid";
+        }
+      }
+      if (!values.password) {
+        errors.password = "Password is required";
+      } else {
+        if (values.password.length < 8)
+          errors.password = "Password must be at least 8 characters";
+      }
+
+      if (!values.confirmPassword) {
+        errors.confirmPassword = "Re-type password";
+      } else {
+        if (values.confirmPassword != values.password)
+          errors.confirmPassword = "Password mismatch";
+      }
+      return errors;
+    },
+  });
+
+  function createAccount({ name, email, password }) {
+    axios
+      .post(
+        API_BACKEND_URL + "/api/create-account",
+        JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      .then((resp) => {
+        const data = resp.data;
+        if (data === "User already in exists") {
+          console.log(data);
+        } else {
+          setShowSuccessMsg(true);
+        }
+      });
+  }
+  return (
+    <>
+      <Navbar />
+      <div className="row g-0 h-100">
+        <div className="col-lg-6 text-center m-auto">
+          <img src={LoginImage} alt="Bank" />
+        </div>
+        <div className="col-lg-6 mx-auto">
+          <Card title="Sign up to create an account.">
+            {showSuccessMsg && (
+              <span className="text-success fw-bold">
+                Your Account has been Created Successfully.
+              </span>
+            )}
+            {!showSuccessMsg ? (
+              <form onSubmit={formik.handleSubmit}>
+                <>
+                  Name
+                  <br />
+                  <input
+                    type="input"
+                    className="form-control"
+                    id="name"
+                    placeholder="Enter name"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                  />
+                  {formik.errors.name && (
+                    <small className="text-danger mb-2">
+                      {formik.errors.name}
+                    </small>
+                  )}
+                  <br />
+                  Email
+                  <br />
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Enter email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                  />
+                  {formik.errors.email && (
+                    <small className="text-danger mb-2">
+                      {formik.errors.email}
+                    </small>
+                  )}
+                  <br />
+                  Password
+                  <br />
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Enter password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                  />
+                  {formik.errors.password && (
+                    <small className="text-danger mb-2">
+                      {formik.errors.password}
+                    </small>
+                  )}
+                  <br />
+                  Confirm Password
+                  <br />
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="confirmPassword"
+                    placeholder="Confirm password"
+                    value={formik.values.confirmPassword}
+                    onChange={formik.handleChange}
+                  />
+                   {formik.errors.confirmPassword && (
+                    <small className="text-danger mb-2">
+                      {formik.errors.confirmPassword}
+                    </small>
+                  )}
+                  <br />
+                  <button type="submit" className="btn btn-primary w-100">
+                    Create Account
+                  </button>
+                </>
+              </form>
+            ) : (
+              <>
+                <button
+                  type="submit"
+                  className="btn btn-light mt-4"
+                  onClick={() => {
+                    formik.handleReset();
+                    setShowSuccessMsg(false);
+                  }}
+                >
+                  Add another account
+                </button>
+              </>
+            )}
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/features/Login/Login.jsx
@@ -0,0 +1,149 @@
+import React, {useEffect, useContext, useState} from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import axios from "axios";
+
+import BankImage from "../../images/bank.svg";
+import Card from "../../components/Card";
+import Navbar from "../../components/Navbar";
+import { UserContext, API_BACKEND_URL } from "../../Context/Context";
+
+export default function Login() {
+    const navigate = useNavigate();
+
+    const [validationMsg, setValidationMsg] = useState(false);
+    const [ctxValue, setCtxValue] = useContext(UserContext);
+    const [redirection, setRedirection] = useState(false);
+
+    useEffect(() => {
+      if (!!ctxValue.username && !!ctxValue.name && !!ctxValue.role && !!ctxValue.accessToken && !!ctxValue.refreshToken)
+       setRedirection(true);
+      else
+        setRedirection(false);
+    }, [JSON.stringify(ctxValue)])
+
+    const formik = useFormik({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      onSubmit: values => {
+        loginUser(values);
+      },
+      validate: values => {
+        let errors = {};
+        if (!values.email) {
+          errors.email = "Email is required";
+        } else {
+          if (
+            !values.email.match(
+              /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )
+          ) {
+            errors.email = "Username should be an email";
+          }
+        }
+        if (!values.password) errors.password = "Password is required";
+        return errors;
+      }
+    })
+
+    if (ctxValue.username && ctxValue.name && ctxValue.role && ctxValue.accessToken && ctxValue.refreshToken) setRedirection(true);
+
+    const loginUser = async ({email, password}) => { 
+      // Set the logged in to true for a user
+      const {data} = await axios.post(API_BACKEND_URL + "/login", {
+          username: email,
+          password: password,
+      });
+    
+      if(data === "Username or password incorrect") {
+          setValidationMsg(true);
+      } else {
+        const accountDetail = await axios.get(API_BACKEND_URL + '/api/account-detail', {
+          headers: {
+            'Authorization': 'Bearer ' + data.accessToken
+          }
+        });
+
+        console.log('Account Detail:', accountDetail);
+
+        setCtxValue({
+          username: data.user.email,
+          name: data.user.name,
+          role: data.user.role,
+          accessToken: data.accessToken,
+          refreshToken: data.refreshToken,
+          accountNumber: accountDetail.data.accountNumber,
+        });
+
+        navigate("/alldata");
+      }
+    }
+  
+    return (
+      <>
+        <Navbar />
+        <div className="row g-0 h-100">
+          <div className="col-lg-6 text-center m-auto">
+            <h1 className="text-primary">Welcome to Smart Bank</h1>
+            <img src={BankImage} alt="Bank" />
+          </div>
+          <div className="col-lg-6 mx-auto">
+            <Card title="Sign on to manage your account">
+             {validationMsg && (
+              <small className="text-danger mb-3">
+                Email/Password is incorrect.
+              </small>
+             )}
+              <form onSubmit={formik.handleSubmit}>
+                <>
+                  Email
+                  <br />
+                  <input
+                    type="input"
+                    className="form-control"
+                    id="email"
+                    placeholder="Enter email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                  />
+                  {formik.errors.email && (
+                    <small className="text-danger mb-3">{formik.errors.email}</small>
+                  )}
+                  <br />
+                  Password
+                  <br />
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Enter password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                  />
+                  {formik.errors.password && (
+                    <small className="text-danger mb-3">{formik.errors.password}</small>
+                  )}
+                  <br />
+                  <button type="submit" className="btn btn-primary w-100">
+                    Login
+                  </button>
+                </>
+              </form>
+              <>
+                <h6 className="text-center mt-4">
+                  Don't have an account? Click here to
+                  <Link to="/create-account" className="d-block">
+                    Create an account
+                  </Link>
+                </h6>
+              </>
+            </Card>
+            {redirection && <Navigate to="/alldata" />}
+          </div>
+        </div>
+      </>
+    );
+  }
+  
